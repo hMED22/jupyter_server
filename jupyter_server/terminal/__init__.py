jupyter/notebook

Add ability to cull terminals and track last activity
This adds functionality to track a terminal's last activity and
optionally cull terminals that have been inactive for some specified
duration.

Resolves: #3868

notebook/terminal/__init__.py
@@ -7,13 +7,14 @@ if not check_version(terminado.__version__, '0.8.1'):
     raise ImportError("terminado >= 0.8.1 required, found %s" % terminado.__version__)
 
 from ipython_genutils.py3compat import which
-from terminado import NamedTermManager
 from tornado.log import app_log
 from notebook.utils import url_path_join as ujoin
+from .terminalmanager import TerminalManager
 from .handlers import TerminalHandler, TermSocket
 from . import api_handlers
 
-def initialize(webapp, notebook_dir, connection_url, settings):
+
+def initialize(webapp, notebook_dir, connection_url, settings, parent):
     if os.name == 'nt':
         default_shell = 'powershell.exe'
     else:
@@ -24,11 +25,12 @@ def initialize(webapp, notebook_dir, connection_url, settings):
     # Enable login mode - to automatically source the /etc/profile script
     if os.name != 'nt':
         shell.append('-l')
-    terminal_manager = webapp.settings['terminal_manager'] = NamedTermManager(
+    terminal_manager = webapp.settings['terminal_manager'] = TerminalManager(
         shell_command=shell,
         extra_env={'JUPYTER_SERVER_ROOT': notebook_dir,
                    'JUPYTER_SERVER_URL': connection_url,
                    },
+        parent=parent,
     )
     terminal_manager.log = app_log
     base_url = webapp.settings['base_url']

notebook/terminal/api_handlers.py
@@ -1,29 +1,19 @@
 import json
 from tornado import web, gen
 from ..base.handlers import APIHandler
-from ..prometheus.metrics import TERMINAL_CURRENTLY_RUNNING_TOTAL
 
 
 class TerminalRootHandler(APIHandler):
     @web.authenticated
     def get(self):
-        tm = self.terminal_manager
-        terms = [{'name': name} for name in tm.terminals]
-        self.finish(json.dumps(terms))
-
-        # Update the metric below to the length of the list 'terms'
-        TERMINAL_CURRENTLY_RUNNING_TOTAL.set(
-            len(terms)
-        )
+        models = self.terminal_manager.list()
+        self.finish(json.dumps(models))
 
     @web.authenticated
     def post(self):
         """POST /terminals creates a new terminal and redirects to it"""
-        name, _ = self.terminal_manager.new_named_terminal()
-        self.finish(json.dumps({'name': name}))
-
-        # Increase the metric by one because a new terminal was created
-        TERMINAL_CURRENTLY_RUNNING_TOTAL.inc()
+        model = self.terminal_manager.create()
+        self.finish(json.dumps(model))
 
 
 class TerminalHandler(APIHandler):
@@ -31,24 +21,12 @@ class TerminalHandler(APIHandler):
 
     @web.authenticated
     def get(self, name):
-        tm = self.terminal_manager
-        if name in tm.terminals:
-            self.finish(json.dumps({'name': name}))
-        else:
-            raise web.HTTPError(404, "Terminal not found: %r" % name)
+        model = self.terminal_manager.get(name)
+        self.finish(json.dumps(model))
 
     @web.authenticated
     @gen.coroutine
     def delete(self, name):
-        tm = self.terminal_manager
-        if name in tm.terminals:
-            yield tm.terminate(name, force=True)
-            self.set_status(204)
-            self.finish()
-
-            # Decrease the metric below by one
-            # because a terminal has been shutdown
-            TERMINAL_CURRENTLY_RUNNING_TOTAL.dec()
-
-        else:
-            raise web.HTTPError(404, "Terminal not found: %r" % name)
+        yield self.terminal_manager.terminate(name, force=True)
+        self.set_status(204)
+        self.finish()

hMED22/jupyter_server

jupyter_server/terminal/__init__.py
@@ -8,14 +8,14 @@ if not check_version(terminado.__version__, '0.8.3'):
     raise ImportError("terminado >= 0.8.3 required, found %s" % terminado.__version__)
 
 from ipython_genutils.py3compat import which
-from terminado import NamedTermManager
 from tornado.log import app_log
 from jupyter_server.utils import url_path_join as ujoin
 from . import api_handlers
-from .handlers import TermSocket
+from .handlers import TerminalHandler, TermSocket
+from .terminalmanager import TerminalManager
 
 
-def initialize(webapp, root_dir, connection_url, settings):
+def initialize(webapp, root_dir, connection_url, settings, parent):
     if os.name == 'nt':
         default_shell = 'powershell.exe'
     else:
@@ -33,11 +33,12 @@ def initialize(webapp, root_dir, connection_url, settings):
     # the user has specifically set a preferred shell command.
     if os.name != 'nt' and shell_override is None and not sys.stdout.isatty():
         shell.append('-l')
-    terminal_manager = webapp.settings['terminal_manager'] = NamedTermManager(
+    terminal_manager = webapp.settings['terminal_manager'] = TerminalManager(
         shell_command=shell,
         extra_env={'JUPYTER_SERVER_ROOT': root_dir,
                    'JUPYTER_SERVER_URL': connection_url,
                    },
+        parent=parent,
     )
     terminal_manager.log = app_log
     base_url = webapp.settings['base_url']

jupyter_server/terminal/api_handlers.py
@@ -1,7 +1,6 @@
 import json
 from tornado import web
 from ..base.handlers import APIHandler
-from ..prometheus.metrics import TERMINAL_CURRENTLY_RUNNING_TOTAL
 
 
 
@@ -9,25 +8,16 @@ class TerminalRootHandler(APIHandler):
 
     @web.authenticated
     def get(self):
-        tm = self.terminal_manager
-        terms = [{'name': name} for name in tm.terminals]
-        self.finish(json.dumps(terms))
-
-        # Update the metric below to the length of the list 'terms'
-        TERMINAL_CURRENTLY_RUNNING_TOTAL.set(
-            len(terms)
-        )
+        models = self.terminal_manager.list()
+        self.finish(json.dumps(models))
 
     @web.authenticated
     def post(self):
         """POST /terminals creates a new terminal and redirects to it"""
         data = self.get_json_body() or {}
 
-        name, _ = self.terminal_manager.new_named_terminal(**data)
-        self.finish(json.dumps({'name': name}))
-
-        # Increase the metric by one because a new terminal was created
-        TERMINAL_CURRENTLY_RUNNING_TOTAL.inc()
+        model = self.terminal_manager.create(**data)
+        self.finish(json.dumps(model))
 
 
 class TerminalHandler(APIHandler):
@@ -35,23 +25,11 @@ class TerminalHandler(APIHandler):
 
     @web.authenticated
     def get(self, name):
-        tm = self.terminal_manager
-        if name in tm.terminals:
-            self.finish(json.dumps({'name': name}))
-        else:
-            raise web.HTTPError(404, "Terminal not found: %r" % name)
+        model = self.terminal_manager.get(name)
+        self.finish(json.dumps(model))
 
     @web.authenticated
     async def delete(self, name):
-        tm = self.terminal_manager
-        if name in tm.terminals:
-            await tm.terminate(name, force=True)
-            self.set_status(204)
-            self.finish()
-
-            # Decrease the metric below by one
-            # because a terminal has been shutdown
-            TERMINAL_CURRENTLY_RUNNING_TOTAL.dec()
-
-        else:
-            raise web.HTTPError(404, "Terminal not found: %r" % name)
+        await self.terminal_manager.terminate(name, force=True)
+        self.set_status(204)
+        self.finish()